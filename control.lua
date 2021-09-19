2407e739a561a4fd1bbd99a289e77b6a91f4a7c7
Nicolas Lang <git@nicolas-lang.de>
2021-09-19 21:32:22 +0200

    code fixes

--- control.lua
+++ control.lua
@@ -4,13 +4,12 @@
 local function remove_blueprints (event)
 	local player = game.players[event.player_index]
 	if player.character ~= nil then
-		trash_inventory = player.get_inventory(defines.inventory.character_trash)
+		local trash_inventory = player.get_inventory(defines.inventory.character_trash)
 		trash_inventory.remove{name="blueprint", count=1}
 		trash_inventory.remove{name="blueprint-book", count=1}
 		trash_inventory.remove{name="deconstruction-planner", count=1}
 		trash_inventory.remove{name="upgrade-planner", count=1}
 	end
-	
 end
 
 script.on_event(defines.events.on_player_trash_inventory_changed,  function(event) remove_blueprints(event) end)
