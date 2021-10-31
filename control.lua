1e781d7af009bef1ff90a7ed76ce88c7ea0de963
Nicolas Lang <git@nicolas-lang.de>
2021-10-31 12:04:57 +0100

    Each trashed blueprint is now exported to the console (factorio_current.log)

--- control.lua
+++ control.lua
@@ -1,15 +1,37 @@
+local function has_value(tab, val)
+	if val and tab then
+		for index, value in ipairs(tab) do
+			if value == val then
+				return true
+			end
+		end
+	end
+	return false
+end
 --===================================================================================================
 -- AutoKill Blueprints in Trash inventory
 --===================================================================================================
-local function remove_blueprints (event)
+local function remove_blueprints(event)
 	local player = game.players[event.player_index]
 	if player.character ~= nil then
 		local trash_inventory = player.get_inventory(defines.inventory.character_trash)
-		trash_inventory.remove{name="blueprint", count=1}
-		trash_inventory.remove{name="blueprint-book", count=1}
-		trash_inventory.remove{name="deconstruction-planner", count=1}
-		trash_inventory.remove{name="upgrade-planner", count=1}
+		for i = 1, #trash_inventory do
+			local stack = trash_inventory[i]
+			if stack.valid_for_read then
+				if has_value({"blueprint", "blueprint-book", "deconstruction-planner", "upgrade-planner"}, stack.name) then
+					local bp_string = stack.export_stack()
+					log(bp_string)
+					--if ((stack.custom_description) and (not string.find(stack.custom_description, "!!!"))) then
+					trash_inventory[i].clear()
+				end
+			end
+		end
 	end
 end
 
-script.on_event(defines.events.on_player_trash_inventory_changed, function(event) remove_blueprints(event) end)
+script.on_event(
+	defines.events.on_player_trash_inventory_changed,
+	(function(event)
+		remove_blueprints(event)
+	end)
+)
